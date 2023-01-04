dc457fcaaf16724e3d73942672f1b02bd0ab3fa3
Andrew Hulterstrom <andrewhulterstrom@gmail.com>
2023-01-04 16:57:28 -0700

    make movie search endpoint return correct data

--- webservice/index.js
+++ webservice/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const dotenv = require('dotenv'); 
-const axios = require('axios');
+const express = require("express");
+const dotenv = require("dotenv");
+const axios = require("axios");
 
 dotenv.config();
 
@@ -8,20 +8,29 @@ const app = express();
 const port = process.env.PORT;
 const token = process.env.API_KEY;
 
-console.log('token', token)
-
-
 const axiosConfig = {
-  headers: { Authorization: `Bearer ${token}` }
+  headers: { Authorization: `Bearer ${token}` },
 };
 
-app.get('/', async (req, res) => {
-  const data = await axios.get( 
-    'https://api.themoviedb.org/3/movie/76341',
-    axiosConfig
-    ).catch(err => console.error('error'))
-  const movie = data.data;
-  res.send(movie);
+app.get("/movies", async (req, res) => {
+  const query = req.query.search;
+
+  const data = await axios
+    .get(
+      `https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&region=US&query=${query}`,
+      axiosConfig
+    )
+    .catch((err) => console.error("error"));
+
+  const formattedData = data?.data?.results.slice(0,10).map((movie) => {
+    return {
+      movie_id: movie.id,
+      title: movie.title,
+      poster_image_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      popularity_summary: `${movie.popularity} out of ${movie.vote_count}`,
+    };
+  });
+  res.send(formattedData);
 });
 
 app.listen(port, () => {
